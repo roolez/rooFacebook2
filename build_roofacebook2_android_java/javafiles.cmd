d38692123dd724b73ff76ddbb89940718958dcc0
My Name <user@user-desktop>
2012-03-27 20:17:04 +0300

    facebook android updated

---
+++ build_roofacebook2_android_java/javafiles.cmd
@@ -0,0 +1,8 @@
+../source/android/src/com/facebook/android/FacebookError.java
+../source/android/src/com/facebook/android/DialogError.java
+../source/android/gen/com/facebook/android/R.java
+../source/android/src/com/facebook/android/Util.java
+../source/android/src/com/facebook/android/FbDialog.java
+../source/android/rooFacebook2.java
+../source/android/src/com/facebook/android/AsyncFacebookRunner.java
+../source/android/src/com/facebook/android/Facebook.java
